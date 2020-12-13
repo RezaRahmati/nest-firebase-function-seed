9db35551013ead69fd747a7c7e63b91c10676367
Reza <Reza.Rahmati@gmail.com>
2020-12-13 11:12:13 -0500

    add firebase

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {ExpressAdapter, NestExpressApplication} from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+import {AppModule} from './app.module';
+
+const server: express.Express = express();
+
+export const createNestServer = async (expressInstance: express.Express): Promise<NestExpressApplication> => {
+	const adapter: ExpressAdapter = new ExpressAdapter(expressInstance);
+	const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(
+		AppModule, adapter, {},
+	);
+
+	app.enableCors();
+
+	return app.init();
+};
+
+createNestServer(server)
+	.then(v => console.log('Nest Ready'))
+	.catch(err => console.error('Nest broken', err));
+
+export const api: functions.HttpsFunction = functions.https.onRequest(server);
